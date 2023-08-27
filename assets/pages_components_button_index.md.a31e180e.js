bdbe37d997955efdba390f373c1c8d3a43bdd59b
lh-js <88662883+lh-js@users.noreply.github.com>
2023-08-27 11:26:18 +0000

    Deploying to build from @ lh-js/knot-vue@4e914d66fcd8de0c4aa37c1e592f30fb117cf835 🚀

---
+++ assets/pages_components_button_index.md.a31e180e.js
@@ -0,0 +1,7 @@
+import{K as e,o as r,c,O as n,w as d,l as p,k as t,a as o}from"./chunks/framework.f50d4957.js";const i=`<template>
+  <kn-button label="点击" />
+</template>
+
+<script setup lang="ts">
+import { KnButton } from "knot-vue";
+<\/script>`,l=t("h1",{id:"button",tabindex:"-1"},[o("button "),t("a",{class:"header-anchor",href:"#button","aria-label":'Permalink to "button"'},"​")],-1),m=t("h2",{id:"基本使用",tabindex:"-1"},[o("基本使用 "),t("a",{class:"header-anchor",href:"#基本使用","aria-label":'Permalink to "基本使用"'},"​")],-1),k=JSON.parse('{"title":"button","description":"","frontmatter":{},"headers":[],"relativePath":"pages/components/button/index.md","filePath":"pages/components/button/index.md","lastUpdated":1693132361000}'),_={name:"pages/components/button/index.md"},x=Object.assign(_,{setup(u){return(b,h)=>{const a=e("kn-button"),s=e("ComponentCard");return r(),c("div",null,[l,m,n(s,{code:"```ts \n"+p(i)},{component:d(()=>[n(a)]),_:1},8,["code"])])}}});export{k as __pageData,x as default};
