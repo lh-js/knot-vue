298cd5fdc6fcde97ce6305f3f06b7cec48c55d2c
lh-js <88662883+lh-js@users.noreply.github.com>
2023-08-28 13:44:56 +0000

    Deploying to build from @ lh-js/knot-vue@6f9765177250aa6b51125e39e08837103f84f583 🚀

--- assets/pages_components_hello_index.md.afd76fcb.lean.js
+++
@@ -1,8 +0,0 @@
-import{K as t,o as s,c as r,O as o,w as c,l as d,k as e,a}from"./chunks/framework.f50d4957.js";const p=`<template>
-  <kn-hello />
-</template>
-
-<script setup lang="ts">
-import { KnHello } from "knot-vue";
-<\/script>
-`,h=e("h1",{id:"hello",tabindex:"-1"},[a("hello "),e("a",{class:"header-anchor",href:"#hello","aria-label":'Permalink to "hello"'},"​")],-1),i=e("h2",{id:"基本使用",tabindex:"-1"},[a("基本使用 "),e("a",{class:"header-anchor",href:"#基本使用","aria-label":'Permalink to "基本使用"'},"​")],-1),x=JSON.parse('{"title":"hello","description":"","frontmatter":{},"headers":[],"relativePath":"pages/components/hello/index.md","filePath":"pages/components/hello/index.md","lastUpdated":1693132361000}'),m={name:"pages/components/hello/index.md"},g=Object.assign(m,{setup(_){return(f,k)=>{const n=t("kn-hello"),l=t("ComponentCard");return s(),r("div",null,[h,i,o(l,{code:"```ts \n"+d(p)},{component:c(()=>[o(n)]),_:1},8,["code"])])}}});export{x as __pageData,g as default};
